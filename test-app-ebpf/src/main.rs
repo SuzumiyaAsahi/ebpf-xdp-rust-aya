5ac289975cb7d2e30ff100ef88ac99dfb8f480d9
SuzumiyaAsahi <784441918@qq.com>
2024-04-05 03:05:39 +0800

    基本实现了比较优雅的停止等待和改进了环形缓冲区的写入策略

--- test-app-ebpf/src/main.rs
+++ test-app-ebpf/src/main.rs
@@ -1,10 +1,11 @@
 #![no_std]
 #![no_main]
 
-use aya_ebpf::cty::c_void;
-use aya_ebpf::helpers::bpf_ringbuf_output;
+//use aya_ebpf::cty::c_void;
+//use aya_ebpf::helpers::{bpf_ringbuf_output, bpf_ringbuf_submit};
 use aya_ebpf::macros::map;
 use aya_ebpf::maps::ring_buf::RingBuf;
+//use aya_ebpf::maps::ring_buf::RingBufEntry;
 use aya_ebpf::maps::HashMap;
 use aya_ebpf::{bindings::xdp_action, macros::xdp, programs::XdpContext};
 use aya_log_ebpf::info;
@@ -67,12 +68,26 @@ fn try_xdp_firewall(ctx: XdpContext) -> Result<u32, ()> {
 
     unsafe {
         let source_addr_be = source_addr.to_be_bytes();
-        bpf_ringbuf_output(
-            &RB as *const RingBuf as *mut core::ffi::c_void,
-            &source_addr_be as *const _ as *mut c_void,
-            core::mem::size_of::<u32>() as u64,
-            0,
-        );
+        if let Some(mut buf) = RB.reserve::<[u8; 4]>(0) {
+            (*buf.as_mut_ptr())[..source_addr_be.len()].copy_from_slice(&source_addr_be[..]);
+            buf.submit(0);
+        } else {
+            info!(&ctx, "RingBuf does not have enough space to store data");
+        }
+
+        //这段代码被上面的代码所替代了，
+        //上面的更安全，
+        //在插入时检查了环形缓冲区是否有足够的空间来存储数据。
+        // This code was replaced by the code above,
+        // which is safer and checks
+        // whether the ring buffer has enough space to store the data during insertion.
+
+        // bpf_ringbuf_output(
+        //     &RB as *const RingBuf as *mut core::ffi::c_void,
+        //     &source_addr_be as *const _ as *mut c_void,
+        //     core::mem::size_of::<u32>() as u64,
+        //     0,
+        // );
         info!(&ctx, "A num is writen");
     };
 
